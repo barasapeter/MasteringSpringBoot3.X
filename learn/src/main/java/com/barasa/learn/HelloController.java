65f8bb1931d094a1a19ae4a7ab7f7647743ee742
barasapeter <barasapeter52@gmail.com>
2025-11-28 12:51:30 +0300

    Learned returning plain strings, returning json, and returning json with status codes

--- learn/src/main/java/com/barasa/learn/HelloController.java
+++ learn/src/main/java/com/barasa/learn/HelloController.java
@@ -1,5 +1,9 @@
 package com.barasa.learn;
 
+import java.util.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
+    public String simpleHelloMessage() {
         return "Hello World!";
     }
+
+    @GetMapping("/hello")
+    public Map<String, String> jsonHelloMessage() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello From Hello with JSON!!");
+        return response;
+    }
+
+    @GetMapping("/hello/with-status-code")
+    public ResponseEntity<Map<String, String>> jsonHelloMessageWithStatusCode() {
+        Map<String, String> returnMessage = new HashMap<>();
+        returnMessage.put("message", "Hello From Hello With Status Code!");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(returnMessage);
+    }
+
 }
